10c1e15275552fb0845c06caf05b1d40808803e4
DiellzaKurteshi <110483734+DiellzaKurteshi@users.noreply.github.com>
2022-09-03 13:16:28 +0200

    after_test

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from pdb import post_mortem
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
-class BlogListView(ListView):
+class BlogListView(ListView):  #klasa na duhet shkaku i inheritance, me trashegu Blog prej ListView ose DetailView
     model = Post 
     template_name = 'home.html'
 
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
